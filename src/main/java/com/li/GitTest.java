0856562b6871bd0df999f487a1aa10024523f799
用户名 <1358796280@qq.com>
2022-08-29 14:50:28 +0800

    hot-fixx first commit

--- src/main/java/com/li/GitTest.java
+++ src/main/java/com/li/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("Hello GitTest3");
         System.out.println("Hello GitTest4");
         System.out.println("Hello GitTest4");
+        System.out.println("Hello GitTest5");
     }
 }
