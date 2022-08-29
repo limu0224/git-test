4e3b5bab67d522e84bdc4cf9196912230952a1c0
用户名 <1358796280@qq.com>
2022-08-29 14:37:39 +0800

    second commit

--- src/main/java/com/li/GitTest.java
+++ src/main/java/com/li/GitTest.java
@@ -3,5 +3,6 @@ package com.li;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello GitTest");
+        System.out.println("Hello GitTest2");
     }
 }
