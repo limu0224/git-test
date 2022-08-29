92264ea1e376908b48c756bade64a966d8e70cf6
用户名 <1358796280@qq.com>
2022-08-29 14:38:12 +0800

    third commit

--- src/main/java/com/li/GitTest.java
+++ src/main/java/com/li/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello GitTest");
         System.out.println("Hello GitTest2");
+        System.out.println("Hello GitTest3");
     }
 }
