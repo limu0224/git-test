7c4822c865e2a57ae7bddbacdf0d9b75113a5526
用户名 <1358796280@qq.com>
2022-08-30 13:57:02 +0800

    push commit

--- src/main/java/com/li/GitTest.java
+++ src/main/java/com/li/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("Hello GitTest5");
         System.out.println("master test");
         System.out.println("hot fix text");
+        System.out.println("push test");
     }
 }
